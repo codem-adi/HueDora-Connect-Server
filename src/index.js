2b6954c56d1e0f027658d4e0daa564c86fb002fe
aditya.singh <emails4adi@gmail.com>
2026-06-15 02:08:09 +0530

    log cahnge

--- src/index.js
+++ src/index.js
@@ -72,7 +72,7 @@ async function start() {
   await ensureDefaultUsers();
   await startEmailPoller();
   app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server is running on port ${PORT} ||`);
   });
 }
 
